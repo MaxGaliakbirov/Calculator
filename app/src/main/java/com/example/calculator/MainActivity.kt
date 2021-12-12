53b65d9af0bfa7cc84ea3360eb4592625866bbda
mf.galiakbirov@gmail.com <n19869400>
2021-12-12 23:04:13 +0300

    add kotlin-android-extensions in plugins;

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        zero_button.setOnClickListener { setTextFields("0")}
+    }
+    fun setTextFields(str: String){
+        math_operation.append(str)
     }
 }
