4c073a32327555019ab5f80775b82bed7b83a7f8
mf.galiakbirov@gmail.com <n19869400>
2021-12-13 23:52:10 +0300

    add event for button AC and BACK

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //добавляем события для кнопок
         zero_button.setOnClickListener { setTextFields("0") }
         one_button.setOnClickListener { setTextFields("1") }
         two_button.setOnClickListener { setTextFields("2") }
@@ -25,6 +25,20 @@ class MainActivity : AppCompatActivity() {
         multiply_button.setOnClickListener { setTextFields("*") }
         plus_button.setOnClickListener { setTextFields("+") }
         minus_button.setOnClickListener { setTextFields("-") }
+        //очищаем поля
+        ac_button.setOnClickListener {
+            math_operation.text = ""
+            result_text.text = ""
+        }
+        //удаляем последний символ
+        back_button.setOnClickListener {
+            val str = math_operation.text.toString() /* добавили переменную str и поместили в неё
+            значение поля math_operation по свойству text и явно приведя его к типу строка */
+            if(str.isNotEmpty()) // isNotEmpty - функция проверки что строка не пустая, возврощает true
+                math_operation.text = str.substring(0, str.length - 1) /* Обрезаем строку начиня с
+                нулевого элемента и заканчивая предпоследним */
+            result_text.text = ""
+        }
     }
     fun setTextFields(str: String){
         math_operation.append(str)
