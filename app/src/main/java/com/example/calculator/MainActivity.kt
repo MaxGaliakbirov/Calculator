75d0a0c93648c3196e5930254d500dd91f698098
mf.galiakbirov@gmail.com <n19869400>
2021-12-13 23:25:22 +0300

    add setOnClickListener for button to 0...9, +, -, *, /, (, )

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,7 +9,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        zero_button.setOnClickListener { setTextFields("0")}
+        zero_button.setOnClickListener { setTextFields("0") }
+        one_button.setOnClickListener { setTextFields("1") }
+        two_button.setOnClickListener { setTextFields("2") }
+        three_button.setOnClickListener { setTextFields("3") }
+        four_button.setOnClickListener { setTextFields("4") }
+        five_button.setOnClickListener { setTextFields("5") }
+        six_button.setOnClickListener { setTextFields("6") }
+        seven_button.setOnClickListener { setTextFields("7") }
+        eight_button.setOnClickListener { setTextFields("8") }
+        nine_button.setOnClickListener { setTextFields("9") }
+        open_bracket_button.setOnClickListener { setTextFields("(") }
+        close_bracket_button.setOnClickListener { setTextFields(")") }
+        divide_button.setOnClickListener { setTextFields("/") }
+        multiply_button.setOnClickListener { setTextFields("*") }
+        plus_button.setOnClickListener { setTextFields("+") }
+        minus_button.setOnClickListener { setTextFields("-") }
     }
     fun setTextFields(str: String){
         math_operation.append(str)
